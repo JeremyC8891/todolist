009a90940a6f22d1e0eed1dab24438305fc97fe0
JeremyC8891 <progo1104@gmail.com>
2026-06-13 14:28:16 +0800

    first commit

---
+++ server.js
@@ -0,0 +1,115 @@
+const http = require('http');
+const { v4: uuidv4 } = require("uuid")
+const errorHandle = require('./errorHandle');
+const todos = [];
+
+const requestListener = function (req, res) {
+  const headers = {
+   'Access-Control-Allow-Headers': 'Content-Type, Authorization, Content-Length, X-Requested-With',
+   'Access-Control-Allow-Origin': '*',
+   'Access-Control-Allow-Methods': 'PATCH, POST, GET,OPTIONS,DELETE',
+   'Content-Type': 'application/json'
+
+  }  
+  
+  let body = '';
+  req.on('data', chunk => {
+    body += chunk;
+  });
+  
+
+  if (req.url === '/todos' && req.method === 'GET') {
+    res.writeHead(200, headers);
+    res.write(JSON.stringify({
+        "status": "success",
+        "data": todos
+    }));
+    res.end();
+  } else if (req.url === '/todos' && req.method === 'POST') {
+      req.on('end', () => {  
+        try {
+          const title = JSON.parse(body).title;
+          if (title !== undefined && title !== '') {
+            const todo = {
+              title,
+              id: uuidv4()
+            };
+            todos.push(todo);
+            console.log(todo);
+            res.writeHead(200, headers);
+            res.write(JSON.stringify({
+                status: "success",
+                data: todos
+            }));
+            res.end();
+          } else {
+            errorHandle(res, 'Invalid request body，請提供 title');
+          }
+        } catch (error) {
+          errorHandle(res, 'Invalid JSON body');
+        }
+      });
+  }else if (req.url === '/todos' && req.method === 'DELETE') {
+      todos.length = 0;
+      res.writeHead(200, headers);
+      res.write(JSON.stringify({
+          "status": "success",
+          "data": todos,
+          "message": "已刪除所有待辦事項"
+      }));
+      res.end();
+  }else if (req.url.startsWith('/todos/') && req.method === 'DELETE') {
+      const id = req.url.split('/').pop();
+      const index = todos.findIndex(todo => todo.id === id);
+      if (index !== -1) {
+        todos.splice(index, 1);
+        res.writeHead(200, headers);
+        res.write(JSON.stringify({
+            "status": "success",
+            "data": todos,
+            
+        }));
+        res.end();
+      }else {
+        errorHandle(res);
+      }
+      console.log(id, index);
+  }else if (req.url.startsWith('/todos/') && req.method === 'PATCH') {
+      req.on('end', () => {
+        try {
+          const todo = JSON.parse(body).title;
+          const id = req.url.split('/').pop();
+          const index = todos.findIndex(todo => todo.id === id);
+          if (todo !== undefined && todo !== -1) {
+              todos[index].title = todo;
+              res.writeHead(200, headers);
+              res.write(JSON.stringify({
+                  "status": "success",
+                  "data": todos,
+                  
+              }));
+              res.end();
+          }else {
+             errorHandle(res);
+          }
+            
+        }catch{
+            errorHandle(res);
+        }
+     });  
+  }else if (req.method === 'OPTIONS') {
+  res.writeHead(200, headers);
+  res.end();
+  } else {
+    res.writeHead(404, headers);
+    res.write(JSON.stringify({
+        "status": "false", 
+        "message": "無此網站"}));
+    res.end();
+  }
+} 
+
+const server = http.createServer(requestListener);  
+
+server.listen(3005);
+console.log('Server running at http://localhost:3005/');
